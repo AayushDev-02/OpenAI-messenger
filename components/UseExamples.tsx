bc4bbbc44a32f3c8fb503d9a1e6a67e89bfbccf9
Aayush Yadav <yadavaayush002@gmail.com>
2023-04-19 17:35:42 +0530

    Made testimonial changes

--- components/UseExamples.tsx
+++ components/UseExamples.tsx
@@ -4,46 +4,46 @@ function UseExamples() {
         <div className="bg-primary mx-20 space-y-3 ">
             <div className="flex space-x-3 ">
                 <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit  border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, fugiat?</p>
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">Explain in detail what is NEXT JS?</h3>
+                    <p className="text-secondary_text">Next.js is a React framework for building production-grade React applications that scale. It provides a powerful set of features that make React development faster and easier. At its core, Next.js is a framework for building server-rendered React applications. </p>
                 </div>
                 <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat consectetur libero, sed repudiandae pariatur consequatur. Facere animi nisi odit quos ipsam. Ex, laborum officia facilis illum odit repellat quibusdam quae fugit deserunt dolorem doloremque at facere, laudantium nisi fuga eos?</p>
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is Github?</h3>
+                    <p className="text-secondary_text">GitHub is a web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and wikis for every project.</p>
                 </div>
                 <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel, architecto quaerat nam sapiente nesciunt impedit eius ratione hic enim commodi accusamus eligendi et, quas iure molestias nobis, dolor aperiam!</p>
-                </div>
-                
-            </div>
-            <div className="flex space-x-3 ">
-                <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel, architecto quaerat nam sapiente nesciunt impedit eius ratione hic enim commodi accusamus eligendi et, quas iure molestias nobis, dolor aperiam!</p>
-                </div>
-                <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel, architecto quaerat nam sapiente nesciunt impedit eius ratione hic enim commodi accusamus eligendi et, quas iure molestias nobis, dolor aperiam!</p>
-                </div>
-                <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel, architecto quaerat nam sapiente nesciunt impedit eius ratione hic enim commodi accusamus eligendi et, quas iure molestias nobis, dolor aperiam!</p>
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">How many campus does vellore institute of technolofy university have ??</h3>
+                    <p className="text-secondary_text">Vellore Institute of Technology University currently has three campuses: its main campus at Vellore, an off-campus centre in Chennai and a second campus at Amaravati, Andhra Pradesh.</p>
                 </div>
                 
             </div>
             <div className="flex space-x-3 ">
                 <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur unde labore dignissimos molestiae? Aspernatur, quas possimus repellendus cumque fugiat qui ipsa atque molestias nihil? Provident temporibus, fugiat dolore quod iste, itaque aliquid earum tenetur non quis reiciendis repellendus adipisci praesentium? Dolore maxime quisquam facere consequatur fugit temporibus quidem, vitae illo.</p>
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">Who is the strongest god in hindu mythology ?</h3>
+                    <p className="text-secondary_text">The strongest god in Hindu mythology is often said to be Lord Shiva. He is the god of destruction, transformation and regeneration. He is also the leader of the Gods and the most powerful being in the universe. He is considered to be the Supreme Being, the source of all life, and the one responsible for the cycle of death and rebirth.</p>
                 </div>
                 <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ab vero iste et. Neque, repellat!</p>
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">Name the first person to visit moon</h3>
+                    <p className="text-secondary_text">The first person to visit the moon was Neil Armstrong, the commander of the Apollo 11 mission. He famously said "That's one small step for man, one giant leap for mankind" when he first set foot on the lunar surface.</p>
                 </div>
                 <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
-                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the color of the sky ?</h3>
-                    <p className="text-secondary_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel, architecto quaerat nam sapiente nesciunt impedit eius ratione hic enim commodi accusamus eligendi et, quas iure molestias nobis, dolor aperiam!</p>
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">Who is the first indian prime minister?</h3>
+                    <p className="text-secondary_text">The first Indian Prime Minister is Jawaharlal Nehru, who was in office from 1947 to 1964.</p>
+                </div>
+                
+            </div>
+            <div className="flex space-x-3 ">
+                <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is the weather today in Jaipur, Rajasthan ?</h3>
+                    <p className="text-secondary_text">The current weather in Jaipur, Rajasthan is sunny with a high of 34°C (93°F) and a low of 26°C (79°F).</p>
+                </div>
+                <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is Tailwind CSS?</h3>
+                    <p className="text-secondary_text">Tailwind CSS is a CSS framework for quickly building custom user interfaces. It utilizes a utility-first approach that allows developers to add classes to HTML elements to apply basic styling. Tailwind provides powerful tools for creating complex, custom designs without having to write long strings of CSS code for every element.</p>
+                </div>
+                <div className="bg-secondary rounded-2xl px-10 py-5 space-y-5 w-fit border-2 border-primary hover:border-secondary_text/20">
+                    <h3 className="text-primary_text font-bold text-2xl underline underline-offset-[10px] decoration-1 decoration-brand">What is Firebase?</h3>
+                    <p className="text-secondary_text">Firebase is a mobile and web application platform with tools and infrastructure designed to help developers build high-quality apps. It provides a real-time database, ready-made authentication and security rules, file storage, notification systems, analytics and much more. Firebase also offers a variety of tools for developers to build applications using Cloud-enabled services, such as Cloud Firestore, Cloud Messaging, and Cloud Functions.</p>
                 </div>
                 
             </div>
